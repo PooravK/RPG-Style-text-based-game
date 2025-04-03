d9ea59bffbeb6e7f40f192eb11996327e6d0932f
PooravK <boomster.work@gmail.com>
2025-04-03 17:57:55 +0530

    Problem 6 of alogorithms of w3resource

--- C++ w3resource/Basic algorithm/6.cpp
+++ C++ w3resource/Basic algorithm/6.cpp
@@ -3,5 +3,31 @@ using namespace std;
 
 int main()
 {
-    
+    int pos, siz;
+    string str1;
+
+    cout << "Enter a string: " << endl;
+    getline(cin, str1);
+    cout << "Which position do you want to get rid of? " << endl;
+    cin >> pos;
+
+    siz = str1.length();
+
+    for (int i = 0; i < siz; i++)
+    {
+        if (i == pos)
+        {
+            str1.erase(i, 1);
+            break;
+        }
+        else
+        {
+            continue;
+        }
+    }
+
+    cout << "New string is: " << endl;
+    cout << str1;
+    return 0;
+
 }
